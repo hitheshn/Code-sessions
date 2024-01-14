898779818f45c32781bfeae240258f6ac02f6ae0
Hithesh N <hithesh143life@gmail.com>
2024-01-14 19:42:27 +0530

    Update referencevariable.cpp

--- referencevariable.cpp
+++ referencevariable.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int a;
-   a=115;
+   int a;//declared variable - 'a'
+   a=115;//assigning value to a
    cout << "a is =" << a << endl;
-   int &b=a;
+   int &b=a;//b is reference variable of a
+   //b can be used instead of a 
+   //any changes made in b will reflect in a
    cout << "b is " << b << endl;
-   b=b*10;
+   b=b*10;//updating value of b
    cout << "a after multiplication " << a << endl;
    cout <<&a<< endl;
    cout <<&b<< endl;
    cout <<a<< endl;
-   cout <<b<< endl;
-   cout <<*a<< endl;
-   
-   cout <<*b<< endl;
+   cout <<b<< enddl;
    return 0;
 }
